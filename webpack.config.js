e2ac4233f9748d4ce3d4f45a16ca25f5c86a61f5
Michael P. Geraci <me@mgeraci.com>
2019-11-04 22:31:11 -0500

    add minimal webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({}),
+  ],
+};
