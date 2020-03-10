13b426db37840152d31e80830d6b7afb2ea76a82
Michael P. Geraci <me@mgeraci.com>
2020-03-10 11:09:07 -0400

    update root div name; update manifest version

--- src/extension/util/constants.js
+++ src/extension/util/constants.js
@@ -1,6 +1,6 @@
 // the root div for fastmail. this element's background color is used as a
 // canary for what theme to use.
-export const FASTMAIL_ROOT_DIV = '.v-Root';
+export const FASTMAIL_ROOT_DIV = 'body';
 
 // the calendar gets added as a sibling to this element
 export const TARGET_DIV = '.v-Quota';
